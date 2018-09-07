dccfdd3e11026ae53d7db1cc699f2228785965bf
asharijuang <asharijuang@me.com>
2018-09-07 22:57:11 +0700

    finish build for cocoapod

--- build.sh
+++ build.sh
@@ -55,7 +55,7 @@ echo "\033[32m  ▹ Create Universal frameworks \033[0m\n"
 cp -RL $IOS_ARCHIVE_FRAMEWORK_PATH $BUILD/$IOS_UNIVERSAL_DIR/$FRAMEWORK_NAME_WITH_EXT
 echo "\033[32m  ▹ Create Universal dSYMs \033[0m\n"
 cp -RL $IOS_ARCHIVE_DSYM_PATH/$DSYM_NAME_WITH_EXT $BUILD/$IOS_UNIVERSAL_DIR/$DSYM_NAME_WITH_EXT
-
+echo "\033[32m  ▹ Create Universal swiftmodule \033[0m\n"
 cp -RL $BUILD/$IOS_SIM_DIR/$FRAMEWORK_NAME_WITH_EXT/Modules/$FRAMEWORK.swiftmodule/* $BUILD/$IOS_UNIVERSAL_DIR/$FRAMEWORK_NAME_WITH_EXT/Modules/$FRAMEWORK.swiftmodule
 echo "\033[35m 🤝 lipo'ing the iOS frameworks together into universal framework \033[0m\n"
 lipo -create $IOS_ARCHIVE_FRAMEWORK_PATH/$FRAMEWORK $BUILD/$IOS_SIM_DIR/$FRAMEWORK_NAME_WITH_EXT/$FRAMEWORK -output $BUILD/$IOS_UNIVERSAL_DIR/$FRAMEWORK_NAME_WITH_EXT/$FRAMEWORK
