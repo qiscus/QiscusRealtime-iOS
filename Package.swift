5083d35a53a15a934094d57e365283d188d43ed7
Arief Nur Putranto <arief@qiscus.co>
2022-08-24 10:59:52 +0700

    update package.swift

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "QiscusRealtime",
-            dependencies: [],
+            dependencies: ["CocoaMQTT"],
             path: "Source"),
     ]
 )
