ba32de9b500de61c395e4014d503046db52973f4
Arief Nur Putranto <arief@qiscus.co>
2022-08-24 10:27:45 +0700

    try support spm

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "QiscusRealtime",
+    platforms: [
+            .macOS(.v10_12),
+            .iOS(.v10),
+    ],
+    products: [
+        .library(
+            name: "QiscusRealtime",
+            targets: ["QiscusRealtime"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "QiscusRealtime",
+            dependencies: [],
+	    path: "Source"),
+        .testTarget(
+            name: "QiscusRealtimeTests",
+            dependencies: ["QiscusRealtime"]),
+    ]
+)
---
+++ Tests/QiscusRealtimeTests/QiscusRealtimeTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import QiscusRealtime
+
+final class QiscusRealtimeTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(QiscusRealtime().text, "Hello, World!")
+    }
+}
